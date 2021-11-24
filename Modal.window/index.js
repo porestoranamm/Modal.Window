58059e12325ba5c065f3820d2d6bd7feb6563810
Nikita Kuznetsov <livisback@yandex.ru>
2021-11-24 16:50:50 +0300

    add options

--- Modal.window/index.js
+++ Modal.window/index.js
@@ -1,1 +1,8 @@
-const myModal = $.modal()
+const modal = $.modal({
+    title: 'Best window modal',
+    closable: true,
+    content: `
+        <h4>Modal is working</h4>
+        <p>Lorem ipsum dolor sit.</p>`,
+        width: '400px'
+})
--- Modal.window/plugins/modal.js
+++ Modal.window/plugins/modal.js
@@ -1,44 +1,53 @@
 function _createModal(options) {
     const modal = document.createElement('div')
+
     modal.classList.add('nmodal')
-    modal.insertAdjacentHTML('afterbegin','
-    <div class="modal-overlay">
-        <div class="modal-window">
-            <div class="modal-header">
-                <div class="modal-title">Modal window</div>
-                <div class="modal-close">
-                    <span>&times;</span>
+
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-overlay">
+            <div class="modal-window">
+                <div class="modal-header">
+                    <div class="modal-title">Modal window</div>
+                    <div class="modal-close">
+                        <span>&times;</span>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+                <div class="modal-footer">
+                    <button>Ok</button>
+                    <button>Cancel</button>
                 </div>
             </div>
-            <div class="modal-body">
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum dolor sit.</p>
-            </div>
-            <div class="modal-footer">
-                <button>Ok</button>
-                <button>Cancel</button>
-            </div>
-        </div>
-    </div>')
+        </div>`)
+
     document.body.appendChild(modal)
+
     return modal
 }
 
 
 
-$.modal = function(options) {
+$.modal = function (options) {
     const ANIMATION_SPEED = 200
-    const $modal = _createModal(options)
+    const modal = _createModal(options)
+    let closing = false
 
-    return{
+    return {
         open() {
-            $modal.classList.add('open')
+           !closing && modal.classList.add('open')
         },
         close() {
+            closing = true
             $modal.classList.remove('open')
             $modal.classList.add('hide')
-            setTimeout(() =>)
+            setTimeout(() =>{
+                $modal.classList.remove('hide')
+                closing = false
+            }, ANIMATION_SPEED)
         },
-        destroy() {}
+        destroy() { }
     }
 }
