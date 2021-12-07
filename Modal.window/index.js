bc2f638210e418bfe02c1756df68ee5449a4c107
Nikita Kuznetsov <livisback@yandex.ru>
2021-12-07 14:37:17 +0300

    transformation html code toys

--- Modal.window/index.js
+++ Modal.window/index.js
@@ -1,7 +1,7 @@
 const toys = [
-    {id: 1, titile: 'Золотая игрушка', price:10, img: 'https://ne-kurim.ru/forum/attachments/1577185158276-png.1085662/'},
-    {id: 2, titile: 'Зеленая игрушка', price:12, img: 'https://ds04.infourok.ru/uploads/ex/0af8/000faedf-6b4f5069/2/hello_html_m49c2fb29.png'},
-    {id: 3, titile: 'Красная игрушка', price:14, img: 'https://img-fotki.yandex.ru/get/4130/16969765.f2/0_6f96c_d34b53bd_orig.png'}
+    {id: 1, title: 'Золотая игрушка', price:10, img: 'https://ne-kurim.ru/forum/attachments/1577185158276-png.1085662/'},
+    {id: 2, title: 'Зеленая игрушка', price:12, img: 'https://ds04.infourok.ru/uploads/ex/0af8/000faedf-6b4f5069/2/hello_html_m49c2fb29.png'},
+    {id: 3, title: 'Красная игрушка', price:14, img: 'https://img-fotki.yandex.ru/get/4130/16969765.f2/0_6f96c_d34b53bd_orig.png'}
 ]
 
 const toHTML = toys => `
