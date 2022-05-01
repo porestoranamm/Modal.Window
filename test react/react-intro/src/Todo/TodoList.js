72423a9d9f5372fd5594e4b0248d6e72d9a80e7b
Nikita Kuznetsov <livisback@yandex.ru>
2022-05-01 16:12:07 +0300

    add style

--- test react/react-intro/src/Todo/TodoList.js
+++ test react/react-intro/src/Todo/TodoList.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import TodoItem from './TodoItem'
 
-export default function TodoList() {
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0,
+    }
+}
+
+function TodoList(props) {
     return (
-        <ul>
-        <li>1</li>
-        <li>2</li>
+        <ul style={styles.ul}>
+        { props.todos.map((todo, index) => {
+            return (
+            <TodoItem 
+            todo={todo} 
+            key={todo.id} 
+            index={index} 
+            onChange={props.onToggle}/>
+            )
+        })}
         </ul>   
-
     )
 }
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
+}
+
+export default TodoList
