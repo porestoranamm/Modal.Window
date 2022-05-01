dc482e9353e2c25f4b476dcfb3d31d4d95ba6ce7
Nikita Kuznetsov <livisback@yandex.ru>
2022-05-01 15:38:35 +0300

    add styles and code items

--- test react/react-intro/src/Todo/TodoItem.js
+++ test react/react-intro/src/Todo/TodoItem.js
@@ -1,5 +1,37 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
-export default function TodoItem() {
-    return <li>todo</li>
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem',
+    },
+    input: {
+        marginRight: '1rem'
+    }
 }
+
+ function TodoItem({ todo }) {
+    return (
+        <li style={styles.li}>
+            <span>
+                <input type="checkbox" style={styles.input}/>
+                <strong>{index + 1}</strong>
+                {todo.title}
+            </span>
+        
+        <button>&times;</button>
+        </li>
+    )
+}
+
+TodoItem.PropTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number
+}
+
+export default TodoItem
